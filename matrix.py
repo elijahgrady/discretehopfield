091695a4fd66555140eeef0df465aa387d604d68
Eli <eligrady@msn.com>
2017-04-08 13:46:33 -0700

    lots of stuff

--- matrix.py
+++ matrix.py
@@ -1,10 +1,10 @@
-# coding=utf-8
-
 import random
 import operator
 import sys
 import unittest
-import math
+
+__version__ = "0.3"
+
 
 class MatrixError(Exception):
     """ An exception class for Matrix """
@@ -73,7 +73,7 @@ class Matrix(object):
         the current matrix """
 
         if self.getRank() != mat.getRank():
-            raise MatrixError, "Trying to add matrices of varying rank!"
+            raise MatrixError, "Trying to add matrixes of varying rank!"
 
         ret = Matrix(self.m, self.n)
 
@@ -231,130 +231,3 @@ class Matrix(object):
 
         rows = listoflists[:]
         return cls._makeMatrix(rows)
-
-"""
-Discrete Hopfield Auto Net
-A well-known iterative auto-associative net (1982)
-Features
-Activation function is discrete
-Symmetric weights,  wij = wji
-No self-connections,  wii = 0
-Asynchronous activation updating
-only one neuron updates at a time and the update is broad-casted to all other neurons
-Continuously taking input signals x
-Guaranteed to converge in theory
-"""
-
-DEBUG = True
-
-# INPUT PARAM: FILENAME
-# RETURN: LIST OF INPUT * OUTPUT {-1,1}
-def readFile(filename):
-    # OPEN THE FILE
-    if DEBUG:
-        f = open('train.txt','r')
-    else:
-        f = open(filename, 'r')
-    # THE FIRST LINE IS AN EMPTY LINE
-    f.readline()
-    # THE NEXT LINE NEEDS TO BE READ IN AS THE INPUT DIMENSIONS
-    input_dim = [int(s) for s in f.readline().split() if s.isdigit()]
-    # THE NEXT LINE IN THE FILE NEEDS TO BE READ IN AS THE OUTPUT DIMENSIONS
-    output_dim = [int(s) for s in f.readline().split() if s.isdigit()]
-    # INIT DATA MEMORY AS A SIMPLE LIST
-    input_data = []
-    # WHILE READLINE WILL PARSE THROUGH ALL THE REST OFF THE LINES IN THE FILE
-    f.readline()
-    for x in range (0, output_dim[0]):
-        for x in range(0, 10):
-            # READ THE ACTUAL LINE
-            line = f.readline()
-            # STRIP ALL OF THE NEWLINE CHARS
-            line = line.strip('\n')
-            # ITERATE THROUGH ALL CHARS IN EVERY LINE
-            for z in line:
-                # CHECK FOR A 'O'
-                if z == 'O':
-                    # CONVERT 'O' TO 1
-                    input_data.append(1)
-                # ELSE WE READ A SPACE
-                elif z == ' ':
-                    # CONVERT SPACE TO -1
-                    input_data.append(-1)
-        # SKIP THE NEXT EMPTY LINE
-        f.readline()
-    return input_data
-
-
-def training():
-    if not DEBUG:
-        in_file = raw_input("Enter training file filename:")
-    else:
-        in_file = None
-    training_data = readFile(in_file)
-    print(training_data)
-    print len(training_data)
-
-
-def testing():
-    return None
-
-
-def ttflag():
-    ttflag = raw_input("whats hoppin? enter 1 to train, anything else to test : ")
-    return ttflag
-
-
-def transpose(matrix):
-    """
-
-    transposed = []
-    for i in range(4):
-...     # the following 3 lines implement the nested listcomp
-...     transposed_row = []
-...     for row in matrix:
-...         transposed_row.append(row[i])
-...     transposed.append(transposed_row)
-    """
-    # SIMPLE ZIP AND UNPACK
-    return list(zip(*matrix))
-
-def main():
-    if (ttflag() == '1'):
-        datalist = training()
-    else:
-        datalist = testing()
-    matrix = Matrix.fromList(datalist)
-    print (matrix)
-
-if __name__ == '__main__':
-    main()
-
-
-
-'''
-Training Hopfield Auto Net
-Hebb rule,
-for a given set of patterns {s1, s2, …, sp} in bipolar,
-weight matrix = s1T s1 + s2T s2 + … + spT sp
-wii = O diagonals
-'''
-
-
-
-"""
-Testing of Hopfield Auto Net
-Use same activation function as iterative auto-associative net
-y = f (yin) = -1  if yin <  0
-y = f (yin) = y  if yin = 0
-y = f (yin) = 1 if yin > 0
-"""
-
-"""
-Testing of Hopfield Auto Net
-1. assume the net has all trained weights (weight matrix W)
-2. for a given test pattern x do
-3. { set yi = xi, i = 1, 2, … n do step 4 (randomly for each neuron i)
-4. yin_i = xi + [y1w1i + y2w2i +…+ ynwni] yi = f (yin_i), i = 1, 2, … n broadcast yi to all other neurons
-5. If (no changes on activation) then converged else set xi = yi and goto step 3 }
-"""
