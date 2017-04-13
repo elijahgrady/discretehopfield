fd25d02f8aeca254e748f20a2fe4f5f19bca936e
Eli <eligrady@msn.com>
2017-04-13 10:56:31 -0700

    TRAINING COMPLETE

--- discretehopfield.py
+++ discretehopfield.py
@@ -1,11 +1,13 @@
 # coding=utf-8
+# alex cameron & eli grady
+# neural nets 2017 USD
 
 import random
 import operator
 import sys
 import unittest
 import math
-from itertools import izip
+from itertools import *
 
 """
 Discrete Hopfield Auto Net
@@ -15,15 +17,17 @@ Activation function is discrete
 Symmetric weights,  wij = wji
 No self-connections,  wii = 0
 Asynchronous activation updating
-only one neuron updates at a time and the update is broad-casted to all other neurons
+Only one neuron updates at a time and the update is broad-casted to all other neurons
 Continuously taking input signals x
 Guaranteed to converge in theory
 """
 
+# SAVES TIME TYPING
 DEBUG = True
 
-# INPUT PARAM: FILENAME
-# RETURN: LIST OF INPUT * OUTPUT {-1,1}
+# INPUT PARAM: FILE PATH TO DATA
+# RETURN: LIST OF X*Z*Z [[[{-1,1}*]]]
+# FUNCTIONS PROPERLY FOR BOTH TRAINING AND TESTING
 def readFile(filename):
     # OPEN THE FILE
     if DEBUG:
@@ -67,73 +71,62 @@ def readFile(filename):
         matrixcontainer.append(matrixbody)
         # RESET BODY
         matrixbody = []
+    # RETURN THE COMPILED LIST OF MATRICES
     return matrixcontainer
 
 
 def training():
+    # CHECK FOR DEBUG OVERRIDE
     if not DEBUG:
         in_file = raw_input("Enter training file filename:")
     else:
         in_file = None
+    # INIT THE TRAINING DATA
     training_data = readFile(in_file)
+    # SEND TRAINING DATA TO MAIN FOR WEIGHT MATRIX CONSTRUCTION
     return training_data
 
 
 def testing():
+    # CHECK FOR DEBUG OVERRIDE
     return None
 
 
 def ttflag():
-    ttflag = raw_input("whats hoppin? enter 1 to train, anything else to test : ")
+    ttflag = raw_input("waz hoppin ... enter 1 to train, 2 to test, anything else to quit : ")
     return ttflag
 
-def epochs():
-    epochs = raw_input("whats hopping? enter the max number of epochs : ")
-    return epochs()
 
-def transpose(matrix):
-    """
-    transposed = []
-    for i in range(4):
-...     # the following 3 lines implement the nested listcomp
-...     transposed_row = []
-...     for row in matrix:
-...         transposed_row.append(row[i])
-...     transposed.append(transposed_row)
-    """
-    # SIMPLE ZIP AND UNPACK
-    return zip(*matrix)
+def saveweightmatrix():
+    swmflag = raw_input("waz hoppin ... enter the name of the file to save the trained weight matrix to : ")
+    return swmflag
+
+
+def saveresults():
+    resflag = raw_input("waz hoppin ... enter the name of the file to save the results of testing to : ")
+    return saveresults()
+
+
+def epochs():
+    epochs = raw_input("waz hoppin ... enter the max number of epochs : ")
+    return epochs()
 
 
 def transpose_matrices(matrixcontainer):
-    # INIT RETURN VAR
-    # NUMBER TO INIT
-    count = matrixcontainer.__len__()
-    count2 = matrixcontainer[0].__len__()
-    # CONTAINER FOR THE TRANSPOSED MATRICES
-    transposed_container = matrixcontainer[:]
-    # LOOP FOR CONTAINER.LEN
-    for z in range(0, count):
-        for i in range(0, count2):
-            for j in range(0, count2):
-                # PERFORM THE TRANSPOSE> NOT SURE WHAT TO DO HERE
-                transposed_container[z][i][j] = matrixcontainer[z][j][i]
-    # RETURN WEIGHT MATRIX
-    return transposed_container
+    return list(map(list, zip(*matrixcontainer)))
 
 
 def add_matrices(c, d):
     return [[a+b for a, b in izip(row1, row2)] for row1, row2 in izip(c, d)]
 
 
-def no_self_connections(weight_matrix):
+def no_self_connections(weightmatrix):
     # SET DIAGONALS TO ZERO
-    count = weight_matrix.__len__()
-    for x in range(0, count):
-        for x in range(0, 10):
-            # SET DIAGONALS TO ZERO HERE
-            weight_matrix[x][x] == 0
-    return weight_matrix
+    count = weightmatrix.__len__()
+    for x in range(0, count, 1):
+        weightmatrix[x][x] = 0
+    return weightmatrix
+
 
 def converged():
     # IF NO CHANGE OR MAX EPOCHS
@@ -143,18 +136,74 @@ def converged():
 def main():
     # TRAINING
     if (ttflag() == '1'):
+        # READ IN THE INPUT FILE AND CONVERT TO BINARY
         matrixcontainer = training()
-        transposedcontainer = transpose_matrices(matrixcontainer)
+        # PROMPT THE USER FOR THE PATH TO SAVE THE WEIGHT MATRIX
+        weightmatrixpath = saveweightmatrix()
+        # TRANSPOSE THE MATRICES FOR WEIGHT MATRIX CONSTRUCTION
+        transposedcontainer = []
+        # SET LOOP COUNTER
+        looper = matrixcontainer.__len__()
+        # CREATE TEMP MATRIX FOR CONVERSIONS
+        t_c = None
+        # LOOP THROUGH MATRIX CONTAINER AND TRANSPOSE EACH N*N MATRIX
+        for x in range(0, looper, 1):
+            t_c = transpose_matrices(matrixcontainer[x])
+            # APPEND TO CONTAINER
+            transposedcontainer.append(t_c)
+
+        '''
+        print("BEFORE TRANSPOSING:")
         print (matrixcontainer)
+        print ("AFTER TRANSPOSING:")
         print (transposedcontainer)
+
+
+        # ^^^ TRANSPOSE IS EITHER NOT WORKING OR THE DATA IS JUST SYMMETRIC ^^^
+        # ^^^ THE TRANSPOSED MATRIX APPEARS TO BE THE SAME AS THE ORIGINAL BUT THE METHOD WORKS ^^^
+        # ^^^ THE LINES BELOW PROVE THE TRANSPOSE METHOD WORKS ^^^
+
+        testmatrix = [[1, 2, 3],
+                      [4, 5, 6],
+                      [7, 8, 9]]
+
+        testmatrix = transpose_matrices(testmatrix)
+
+        print (" THIS PROVES THE TRANSPOSE METHOD WORKS")
+
+        print(testmatrix)
+
+        # WILL PRINT A CORRECTLY TRANSPOSED MATRIX
+
         '''
-        weight_matrix = add_matrices(transposedcontainer, matrixcontainer)
-        weight_matrix = no_self_connections(weight_matrix)
-        '''
+
+        # ADD THE MATRICES TOGETHER FOR WEIGHT MATRIX CONSTRUCTION
+        weightcontainer = []
+        w_c = None
+        for x in range(0, looper, 1):
+            w_c = add_matrices(transposedcontainer[x], matrixcontainer[x])
+            weightcontainer.append(w_c)
+        # LOOP THROUGH N MATRICES TO BUILD THE FINAL COMPILED WEIGHT MATRIX
+        for x in range(1, looper, 1):
+            weightcontainer[0] = add_matrices(weightcontainer[0], weightcontainer[x])
+        weightmatrix = weightcontainer[0]
+        # SET SELF CONNECTIONS OF WEIGHT MATRIX TO 0
+        weightmatrix = no_self_connections(weightmatrix)
+        # OUTPUT AND SAVE THE FINAL WEIGHT MATRIX
+        print ("testing complete, below is the weight matrix: ")
+        print (weightmatrix)
+        # SAVE THE WEIGHT MATRIX TO COMPLETE TRAINING
+        f = open(weightmatrixpath, "a+")
+        f.write(str(weightmatrix))
+        f.close()
+        # TRAINING COMPLETE
 
     # TESTING
     else:
         matrixcontainer = testing()
+        # PROMPT THE USER TO GIVE US THE FILE WHERE THE TRAINING DATA IS SAVED
+        # PROMPT THE USER TO GIVE US THE FILE WHERE THE TESTING DATA IS SAVED
+        # RETURN THE RESULTS OF THE TESTING
 
 
 if __name__ == '__main__':
